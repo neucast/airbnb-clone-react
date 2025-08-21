af6d79b551720779a1ea7fea423276f720d5cc6c
Luis Neumann <luisgneumannc@hotmail.com>
2025-08-21 13:13:57 -0600

    Added AbortController to stop unnecessary fetchs to the API. Luis Neumann. August 21st., 2025.

--- src/features/properties/ui/PropertyGrid/PropertyGrid.tsx
+++ src/features/properties/ui/PropertyGrid/PropertyGrid.tsx
@@ -1,19 +1,19 @@
-import {PropertyCard} from "../PropertyCard";
-import {useEffect, useState} from "react";
+import { PropertyCard } from "../PropertyCard";
+import { useEffect, useMemo, useState } from "react";
 import './PropertyGrid.css'
-import {usePropertiesStore} from "../../model/store";
-import {type Property} from "../../model/types";
-import {PropertiesApi} from "../../api/propertiesApi.ts";
+import { usePropertiesStore } from "../../model/store";
+import { type Property } from "../../model/types";
+import { PropertiesApi } from "../../api/propertiesApi";
 
 function PropertyGrid() {
     const loadProperties = usePropertiesStore((state) => state.loadProperties);
     const properties = usePropertiesStore((state) => state.properties);
-    // const sortedProperties = usePropertiesStore((state) => state.sortedProperties);
+    //const sortedProperties = usePropertiesStore((state) => state.sortedProperties);
     const loading = usePropertiesStore((state) => state.loading);
     const searchTerm = usePropertiesStore((state) => state.searchTerm);
-    // const searchProperties = usePropertiesStore((state) => state.searchProperties);
+    //const searchProperties = usePropertiesStore((state) => state.searchProperties);
 
-    const [sortedProperties, setSortedProperties] = useState<Property[]>([])
+    const [sortedProperties, setSortedProperties] = useState<Property[]>([]);
 
     useEffect(() => {
         loadProperties();
@@ -24,38 +24,29 @@ function PropertyGrid() {
     }, [properties]);
 
     useEffect(() => {
-
-        if (searchTerm.trim() === '') {
-            setSortedProperties(properties);
-            return;
-        }
-
         const controller = new AbortController();
-
         const searchProperties = async () => {
             const properties = await PropertiesApi.searchProperties(searchTerm, controller.signal);
             setSortedProperties(properties);
         }
-
         searchProperties();
-
+        
         return () => {
             controller.abort();
         }
-
-        // searchProperties(searchTerm);
     }, [searchTerm]);
+
 
     if (loading) {
         return <div>Loading...</div>;
     }
 
     return (
-        <div className="property-grid-container">
+        <div className="property-grid-container"> 
             <h1>Popular Destinations</h1>
             <div className="property-grid">
                 {sortedProperties.map((property: Property) => (
-                    <PropertyCard
+                    <PropertyCard 
                         key={property.id}
                         {...property}
                     />
