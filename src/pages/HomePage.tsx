8c1af7157ba09684e72f974a2078de774c47299e
Luis Neumann <luisgneumannc@hotmail.com>
2025-08-27 13:46:03 -0600

    Added push messages to the application.

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,19 @@
-import { PropertyGrid } from "../features/properties";
-import { SearchBar } from "../features/properties";
-import { useAuth } from "../shared/hooks/useAuth";
+import {PropertyGrid, SearchBar} from "../features/properties";
+import {useAuth} from "../shared/hooks/useAuth";
+import OneSignal from "react-onesignal";
+import {useEffect} from "react";
 
 
 function HomePage() {
-    const { login } = useAuth()
+    useEffect(() => {
+        OneSignal.init({
+                appId: "42c9d5b7-9e03-4de2-bd89-477d9caa5a83",
+                allowLocalhostAsSecureOrigin: true,
+            }
+        );
+    }, []);
+
+    const {login} = useAuth()
 
     const handleLogin = () => {
         login()
@@ -12,8 +21,8 @@ function HomePage() {
 
     return (
         <>
-            <SearchBar />
-            <PropertyGrid />
+            <SearchBar/>
+            <PropertyGrid/>
             <div>
                 <button onClick={handleLogin}>
                     Login
